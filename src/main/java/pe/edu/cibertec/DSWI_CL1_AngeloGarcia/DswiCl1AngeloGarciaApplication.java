66c95e33d1165a5eaee1265add1ca302a617569b
AngeloGarcia <guioangelo.garcia@gmail.com>
2024-06-08 12:36:47 -0500

    primer commit

---
+++ src/main/java/pe/edu/cibertec/DSWI_CL1_AngeloGarcia/DswiCl1AngeloGarciaApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWI_CL1_AngeloGarcia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiCl1AngeloGarciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiCl1AngeloGarciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DSWI_CL1_AngeloGarcia/DswiCl1AngeloGarciaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DSWI_CL1_AngeloGarcia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiCl1AngeloGarciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
